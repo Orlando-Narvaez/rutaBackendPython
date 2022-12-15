2d94499845c82968a96e380be7e9f6811f5dd9b3
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-15 17:21:50 -0500

    Create 07_transformation.py

---
+++ fundamentosPython/07_transformation.py
@@ -0,0 +1,18 @@
+name = "Orlando"
+print(type(name))
+name = 12
+print(type(name))
+name = True
+print(type(name))
+
+print("Orlando " + " Narvaez")
+print(10+2)
+print("Orlando " + " 23" )
+
+age = 23
+print("Mi edad es: " + str(age))
+print(f"Mi edad es: {age}")
+
+age = int(input("Cual es tu edad? "))
+print(f"Tu edad en 10 años es: {age + 10}")
+

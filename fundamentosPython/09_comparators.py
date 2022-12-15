1b454d2a63b09d353f2d675df1cda7f55558bd74
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-15 17:43:26 -0500

    Create 09_comparators.py

---
+++ fundamentosPython/09_comparators.py
@@ -0,0 +1,37 @@
+# >
+print(7 > 3)
+print(7 > 9)
+print(7 > 7)
+
+# <
+print(7 < 3)
+print(7 < 9)
+print(7 < 7)
+
+#>=
+print(7 >= 3)
+print(7 >= 9)
+print(7 >= 7)
+
+# <=
+print(7 <= 3)
+print(7 <= 9)
+print(7 <= 7)
+
+# ==
+print(7 == 3)
+print(7 == 9)
+print(7 == 7)
+
+# !=
+print(7 != 3)
+print(7 != 9)
+print(7 != 7)
+
+print("Apple" == 'Apple')
+print("Apple" == 'apple')
+
+print("1" == 1)
+
+age = 18
+print(age >= 18)

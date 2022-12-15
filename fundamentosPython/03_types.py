47dded43bf3df5810de34b27c51c24bd0e2fda32
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-15 16:58:21 -0500

    Create 03_types.py

---
+++ fundamentosPython/03_types.py
@@ -0,0 +1,23 @@
+#String
+myName = "Orlando"
+myName = 'Orlando'
+print("Variable string: " + myName)
+#Para saber que tipo es la variable
+print(type(myName))
+
+#Int
+myAge = 23
+print("My age = " + str(myAge))
+print(type(myAge))
+
+#Boolean
+isSingle = False
+print("Is single = " + str(isSingle))
+print(type(isSingle))
+
+#Inputs
+myAge = int(input("Cuel es tu edad? "))
+print(type(myAge))
+
+
+

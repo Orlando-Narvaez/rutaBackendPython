9ed2f1556eb7b6cc20160048e2a60f94cc9c5df1
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-15 18:13:24 -0500

    Create 12_if.py

---
+++ fundamentosPython/12_if.py
@@ -0,0 +1,12 @@
+if True:
+    print("Deberia ejecutarse")
+
+if False:
+    print("Nunca deberia ejecutarse")
+
+num = int(input("Ingrese un numero: "))
+
+if num % 2 == 0:
+    print("El numeor es par")
+else:
+    print("El numero es impar")

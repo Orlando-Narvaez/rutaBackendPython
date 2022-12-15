e7291aa313e939d13feb57cd598d9c7df2ec4d7f
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-14 22:33:46 -0500

    Create 02_vars.py

---
+++ fundamentosPython/02_vars.py
@@ -0,0 +1,16 @@
+print("Hola, vars")
+
+#Variable con un String
+myName = "Orlando"
+print(myName)
+
+#Variable con un numero
+myAge = 23
+print(myAge)
+
+myName = "Chacho"
+print("Aqui cambio el nombre a: " + myName)
+
+#input
+myName = input("Cual es tu nombre? ")
+print("Usando input: " + myName)

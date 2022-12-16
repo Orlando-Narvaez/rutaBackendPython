a9c8877cbd6086ac755cc9900b3f61177d13fce0
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-16 12:01:40 -0500

    Update rockPaperScissors.py
    Mejora del código de piedra papel o tijeras

--- fundamentosPython/rockPaperScissors.py
+++ fundamentosPython/rockPaperScissors.py
@@ -1,29 +1,60 @@
 import random
 
 print("""
-Bienvenido al juego piedra, papel o tijeras
-para seleccionar una opcion tener en cuenta:
-1 - Piedra
-2 - Papel
-3 - Tijeras
-""")
-userOption = int(input("Escoge una opcion: entre el 1 y el 3: "))
-computerOpcion = random.randint(1, 3) #para strings se usa: choice
+    Bienvenido al juego piedra, papel o tijeras
+    para seleccionar una opcion tener en cuenta:
+    1 - Piedra
+    2 - Papel
+    3 - Tijeras
 
-if userOption == computerOpcion:
-    print("Empate!")
-elif userOption == 1:
-    if computerOpcion == 2:
-        print("Has perdido, la maquina escogio Papel")
+    Gana el mejor de 3\n
+    """)
+
+rounds = 1
+pointPerson = 0
+pointComputer = 0
+
+while True:
+    print("*" *10)
+    print("ROUND ", rounds)
+    print("*" *10)
+    print("\nLa maquina lleva: ", pointComputer, " puntos\nUsted lleva: ", pointPerson, " puntos")
+    userOption = 0
+    while userOption <= 0 or userOption >=4:
+        userOption = int(input("\nEscoge una opcion: entre el 1 = (Piedra), 2 = (Papel) y el 3 = (Tijeras) : "))
+        if userOption <= 0 or userOption >=4:
+            print("\nIngrese un valor valido por favor")
+    computerOpcion = random.randint(1, 3) #para strings se usa: choice
+
+    if userOption == computerOpcion:
+        print("\nEmpate!")
+    elif userOption == 1:
+        if computerOpcion == 2:
+            print("\nHas perdido, la maquina escogio Papel")
+            pointComputer += 1
+        else:
+            print("\nHas ganado, la maquina escogio Tijeras")
+            pointPerson += 1
+    elif userOption == 2:
+        if computerOpcion == 1:
+            print("\nHas ganado, la maquina escogio Piedra")
+            pointPerson += 1
+        else:
+            print("\nHas perdido, la maquina escogido Tijeras")
+            pointComputer += 1
+    elif userOption == 3:
+        if computerOpcion == 1:
+            print("\nHas perdido, la maquina escogio Piedra")
+            pointComputer += 1
+        else:
+            print("\nHas ganado, la maquina escogido papel")
+            pointPerson += 1
+    
+    if pointPerson == 2:
+        print("\nFelicidades, has ganado el juego!")
+        break
+    elif pointComputer == 2:
+        print("\nVaya has perdido, gano la maquina, suerte a la proxima")
+        break
     else:
-        print("Has ganado, la maquina escogio Tijeras")
-elif userOption == 2:
-    if computerOpcion == 1:
-        print("Has ganado, la maquina escogio Piedra")
-    else:
-        print("Has perdido, la maquina escogido Tijeras")
-elif userOption == 3:
-    if computerOpcion == 1:
-        print("Has perdido, la maquina escogio Piedra")
-    else:
-        print("Has ganado, la maquina escogido papel")
+        rounds += 1

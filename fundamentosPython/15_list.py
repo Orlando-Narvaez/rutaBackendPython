37a5b82d7453057f820b4cb81eb4ebc893d0e8fd
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-16 09:25:49 -0500

    Create 15_list.py

---
+++ fundamentosPython/15_list.py
@@ -0,0 +1,21 @@
+numbers = [1,2,3,4,5]
+print(numbers)
+print(type(numbers))
+
+tasks = ["Lavar plato", "Jugar vidojuegos"]
+print(tasks)
+
+types = [1, True, "Hola"]
+print(types)
+
+print(numbers[0])
+print(tasks[1])
+
+text = "Hola"
+
+tasks[0] = "Ver clases de Platzi"
+print(tasks)
+
+print(numbers[:3])
+
+print(True in types)

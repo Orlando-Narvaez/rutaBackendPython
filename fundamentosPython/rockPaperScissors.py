150db0fbc8411e836f6d5bc714b2417c8bda7bdd
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-15 21:29:06 -0500

    Create rockPaperScissors.py

---
+++ fundamentosPython/rockPaperScissors.py
@@ -0,0 +1,29 @@
+import random
+
+print("""
+Bienvenido al juego piedra, papel o tijeras
+para seleccionar una opcion tener en cuenta:
+1 - Piedra
+2 - Papel
+3 - Tijeras
+""")
+userOption = int(input("Escoge una opcion: entre el 1 y el 3: "))
+computerOpcion = random.randint(1, 3)
+
+if userOption == computerOpcion:
+    print("Empate!")
+elif userOption == 1:
+    if computerOpcion == 2:
+        print("Has perdido, la maquina escogio Papel")
+    else:
+        print("Has ganado, la maquina escogio Tijeras")
+elif userOption == 2:
+    if computerOpcion == 1:
+        print("Has ganado, la maquina escogio Piedra")
+    else:
+        print("Has perdido, la maquina escogido Tijeras")
+elif userOption == 3:
+    if computerOpcion == 1:
+        print("Has perdido, la maquina escogio Piedra")
+    else:
+        print("Has ganado, la maquina escogido papel")

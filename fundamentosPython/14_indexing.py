cc79dfffe0918417049e3652ea119bb24df08a6b
Orlando-Narvaez <orlandonarvaezb@gmail.com>
2022-12-15 23:02:11 -0500

    Create 14_indexing.py

---
+++ fundamentosPython/14_indexing.py
@@ -0,0 +1,14 @@
+text = "Ella sabe Python"
+print(text[0])
+print(text[1])
+
+size = len(text)
+print(text[size - 1])
+print(text[-1])
+
+# Slicing
+
+print(text[0:5])
+print(text[:10])
+print(text[::-1])
+print(text[1:16:2])

cf47bc8f66d7043c381129f92d6eda1c3826ddd4
Orlando Narvaez <orlandonarvaezb@gmail.com>
2022-12-21 12:27:46 -0500

    Update main.py
    Aprendiendo conceptos de paquetes

--- cursoPython_comprehensionsFuncionesManejoErrores/Python 102/main.py
+++ cursoPython_comprehensionsFuncionesManejoErrores/Python 102/main.py
@@ -1,10 +1,11 @@
-import this
+"""from pkg.mod1 import func1, func2
+from pkg.mod2 import func3, func4
 
+print(func1())
+print(func2())
+print(func3())
+print(func4())"""
 
-
-#def run():
-#    pass
-
-
-#if __name__ == '__main__':
-#    run()
+import pkg
+print(pkg.URL)
+print(pkg.mod1.func1())

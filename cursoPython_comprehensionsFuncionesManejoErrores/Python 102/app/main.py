da03bbbbef14a49281c34c47fc87d91851a38d63
Orlando Narvaez <orlandonarvaezb@gmail.com>
2022-12-21 12:02:16 -0500

    Update main.py
    Aprendiendo conceptos de modulos como scripts

--- cursoPython_comprehensionsFuncionesManejoErrores/Python 102/app/main.py
+++ cursoPython_comprehensionsFuncionesManejoErrores/Python 102/app/main.py
@@ -1,19 +1,24 @@
 import utils 
-keys,values = utils.getPopulation()
-print(keys,values)
 
-data = [
-    {
-        "Country": "Colombia",
-        "Population": 300
-    },
-    {
-        "Country": "Bolivia",
-        "Population": 100
-    }
-]
+def run():
+    keys,values = utils.getPopulation()
+    print(keys,values)
 
-country = input("Escribe el pais que desea buscar: ")
+    data = [
+        {
+            "Country": "Colombia",
+            "Population": 300
+        },
+        {
+            "Country": "Bolivia",
+            "Population": 100
+        }
+    ]
 
-result = utils.populationByCountry(data, country)
-print(result)
+    country = input("Escribe el pais que desea buscar: ")
+
+    result = utils.populationByCountry(data, country)
+    print(result)
+
+if __name__ == '__main__':
+    run()

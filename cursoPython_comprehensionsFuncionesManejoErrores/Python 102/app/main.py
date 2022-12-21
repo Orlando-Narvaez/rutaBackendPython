effa8441486e8489781a9bae28bdb3e4b0b65294
Orlando Narvaez <orlandonarvaezb@gmail.com>
2022-12-21 17:54:40 -0500

    Update main.py
    Completando reto platzi de grafica de torta

--- cursoPython_comprehensionsFuncionesManejoErrores/Python 102/app/main.py
+++ cursoPython_comprehensionsFuncionesManejoErrores/Python 102/app/main.py
@@ -4,11 +4,11 @@ import charts
 
 def run():
   data = readCSS.read_csv('./data.csv')
-  """data = list(filter(lambda item : item['Continent'] == 'South America',data))
+  data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
   countries = list(map(lambda x: x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
-  charts.generateBarChart(countries, percentages)
+  charts.generatePieChart(countries, percentages)
   """
   country = input('Type Country => ')
 
@@ -17,7 +17,7 @@ def run():
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
-    charts.generateBarChart(labels, values)
+    charts.generateBarChart(labels, values)"""
 
 if __name__ == '__main__':
   run()

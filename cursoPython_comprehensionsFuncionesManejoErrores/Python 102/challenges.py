0466fa9fe65670aa7ce87ad80698724b9ec20700
Orlando Narvaez <orlandonarvaezb@gmail.com>
2022-12-20 21:49:26 -0500

    Update challenges.py

--- cursoPython_comprehensionsFuncionesManejoErrores/Python 102/challenges.py
+++ cursoPython_comprehensionsFuncionesManejoErrores/Python 102/challenges.py
@@ -27,7 +27,7 @@ def message_creator(text):
 
 text = 'computadora'
 response = message_creator(text)
-print(response)"""
+print(response)
 
 def message_creator(text):
     # Escribe tu solución 👇
@@ -42,4 +42,13 @@ def message_creator(text):
 
 text = "computadora"
 response = message_creator(text)
+print(response)"""
+
+def multiply_numbers(numbers):
+    # Escribe tu solución 👇
+    result = list(map(lambda number: number * 2, numbers))
+    return result
+
+numbers = [1, 2, 3, 4]
+response = multiply_numbers(numbers)
 print(response)

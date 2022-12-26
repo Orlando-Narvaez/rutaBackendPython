fe85be718106f00dbd2d85046c6ab1603bbc73f9
Orlando Narvaez <orlandonarvaezb@gmail.com>
2022-12-26 15:29:48 -0500

    Create main.py

---
+++ cursoPython_PIP_EntornosVirtuales/app/main.py
@@ -0,0 +1,23 @@
+import utils
+import readCSS
+import charts
+
+def run():
+  data = readCSS.read_csv('./data.csv')
+  data = list(filter(lambda item : item['Continent'] == 'South America',data))
+
+  countries = list(map(lambda x: x['Country/Territory'], data))
+  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  charts.generatePieChart(countries, percentages)
+  """
+  country = input('Type Country => ')
+
+  result = utils.population_by_country(data, country)
+
+  if len(result) > 0:
+    country = result[0]
+    labels, values = utils.get_population(country)
+    charts.generateBarChart(labels, values)"""
+
+if __name__ == '__main__':
+  run()

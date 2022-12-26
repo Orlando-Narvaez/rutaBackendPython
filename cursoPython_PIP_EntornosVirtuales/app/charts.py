01ba9931661aaf3217015d4a6da628787a31ba69
Orlando Narvaez <orlandonarvaezb@gmail.com>
2022-12-26 15:55:52 -0500

    Update charts.py
    Aprendiendo conceptos de graficas en Python con PIP

--- cursoPython_PIP_EntornosVirtuales/app/charts.py
+++ cursoPython_PIP_EntornosVirtuales/app/charts.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 
-def generateBarChart(labels,values): #Grafica de barras
+def generateBarChart(name,labels,values): #Grafica de barras
     fig, ax = plt.subplots()
     ax.bar(labels,values)
-    plt.show()
+    plt.savefig(f"./imgs/{name}.png")
+    plt.close()
 
 def generatePieChart(labels,values): #Grafica de pastel
     fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
     ax.axis("equal")
-    plt.show()
+    plt.savefig("./imgs/pie.png")
+    plt.close()
 
 if __name__  == "__main__":
     labels = ["a", "b", "c"]

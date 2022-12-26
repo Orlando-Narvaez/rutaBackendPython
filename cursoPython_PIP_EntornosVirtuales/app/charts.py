40a6357f0d849f639d3b348250620d3d05617574
Orlando Narvaez <orlandonarvaezb@gmail.com>
2022-12-26 15:29:39 -0500

    Create charts.py

---
+++ cursoPython_PIP_EntornosVirtuales/app/charts.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+def generateBarChart(labels,values): #Grafica de barras
+    fig, ax = plt.subplots()
+    ax.bar(labels,values)
+    plt.show()
+
+def generatePieChart(labels,values): #Grafica de pastel
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    ax.axis("equal")
+    plt.show()
+
+if __name__  == "__main__":
+    labels = ["a", "b", "c"]
+    values = [100, 200, 80]
+    generateBarChart(labels,values)
+    generatePieChart(labels,values)
